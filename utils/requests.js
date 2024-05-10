78c14992ff3dba687c869124ab6f235bfd2eb264
zakaria bouregbi <zakariabouregbidev@gmail.com>
2024-05-10 15:18:19 +0100

    edit request.js

--- utils/requests.js
+++ utils/requests.js
@@ -1,62 +1,48 @@
-//const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null
+const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
-async function fetchProperties({showFeatured = false} = {}){
-
-    try
-
-    {
-        //handle where the domain is not available yet
-      
-      const res = await fetch (`http://localhost:3000/api/properties${showFeatured ? '/featured' : ''}`,{cache:'no-store'});
-
-      
-      if (!res.ok) 
-      {
-        throw new Error('failed to fetch data')
-      }
-  
-      return res.json()
-  
+// Fetch all properties
+async function fetchProperties({ showFeatured = false } = {}) {
+  try {
+    // Handle the case where the domain is not available yet
+    if (!apiDomain) {
+      return [];
     }
-  
-    catch(error){
 
-      console.log(error)
+    const res = await fetch(
+      `${apiDomain}/properties${showFeatured ? '/featured' : ''}`,
+      { cache: 'no-store' }
+    );
 
+    if (!res.ok) {
+      throw new Error('Failed to fetch data');
     }
-  
+
+    return res.json();
+  } catch (error) {
+    console.log(error);
+    return [];
   }
+}
 
-  //Fetch single Property
-
-  async function fetchProperty(id){
-
-    try
-
-    {
-        //handle where the domain is not available yet
-      
-  
-      const res = await fetch (`http://localhost:3000/api/properties/${id}`);
-  
-      if (!res.ok) 
-      {
-        throw new Error('failed to fetch data')
-  
-      }
-  
-      return res.json()
-  
+// Fetch single property
+async function fetchProperty(id) {
+  try {
+    // Handle the case where the domain is not available yet
+    if (!apiDomain) {
+      return null;
     }
-  
-    catch(error){
-      console.log(error)
-      return null
-  
+
+    const res = await fetch(`${apiDomain}/properties/${id}`);
+
+    if (!res.ok) {
+      throw new Error('Failed to fetch data');
     }
-  
+
+    return res.json();
+  } catch (error) {
+    console.log(error);
+    return null;
   }
+}
 
-
-  
-  export { fetchProperties,fetchProperty };
+export { fetchProperties, fetchProperty };
